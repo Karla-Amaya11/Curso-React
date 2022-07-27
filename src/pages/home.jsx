5e7413ce9a7e656857c4d73e4fd002e0bb872137
julio aleman <javierarb7530@gmail.com>
2022-07-27 10:54:33 -0600

    agregando mi primer repositorio

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import headers from './layouts/headers'
+
+const home = () => {
+  return (
+    <div>home
+        <Headers></Headers>
+    </div>
+  )
+}
+
+export default home
